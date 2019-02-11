48099873a5f401cfb97ecfaf8505cdbfc99ded87
Marko Rusic <markousp5@gmail.com>
2019-02-12 00:34:43 +0100

    added and configured material bottom tabs & vector icons

--- src/App.js
+++ src/App.js
@@ -2,17 +2,43 @@ import React from 'react'
 import {
   createSwitchNavigator,
   createStackNavigator,
-  createBottomTabNavigator,
   createAppContainer
 } from 'react-navigation'
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import { Text } from 'react-native'
+import { PRIMARY_COLOR } from './config/colors'
 import AuthLoadingScreen from './screens/AuthLoadingScreen'
 import SignInScreen from './screens/SignInScreen'
 
-const AppStack = createBottomTabNavigator({
-  Search: () => <Text>Market Search</Text>,
-  Favorites: () => <Text>Favorites</Text>
-})
+const AppStack = createMaterialBottomTabNavigator(
+  {
+    Search: {
+      screen: () => <Text>Market Search</Text>,
+      navigationOptions: {
+        tabBarLabel: 'Market Search',
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="view-list" size={25} color={tintColor} />
+        )
+      }
+    },
+    Favorites: {
+      screen: () => <Text>Favorites</Text>,
+      navigationOptions: {
+        tabBarLabel: 'Favorites',
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="heart" size={25} color={tintColor} />
+        )
+      }
+    }
+  },
+  {
+    initialRouteName: 'Search',
+    activeColor: PRIMARY_COLOR,
+    shifting: true,
+    barStyle: { backgroundColor: 'white' }
+  }
+)
 const AuthStack = createStackNavigator({ SignIn: SignInScreen })
 
 const AppContainer = createAppContainer(
