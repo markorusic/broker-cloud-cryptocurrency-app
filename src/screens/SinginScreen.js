2b49578dca11ff9bb322e2911509a041544f34fc
Marko Rusic <markousp5@gmail.com>
2019-02-11 17:46:27 +0100

    SinginScreen base UI

--- src/screens/SinginScreen.js
+++ src/screens/SinginScreen.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
+import ScreenContainer from 'app/shared/components/ScreenContainer'
+import TextField from 'app/shared/components/TextField'
+import Button from 'app/shared/components/Button'
 
 export default class SinginScreen extends Component {
+  static navigationOptions = {
+    header: null
+  }
+
   render() {
     return (
-      <View>
-        <Text> Sing in </Text>
-      </View>
+      <ScreenContainer style={styles.container}>
+        <Text style={styles.heading}>Wellcome</Text>
+        <View style={styles.fullWidth}>
+          <TextField label="Email" />
+          <TextField label="Password" />
+        </View>
+        <Button title="Sing in" uppercase />
+      </ScreenContainer>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 100,
+    paddingHorizontal: 50
+  },
+  heading: {
+    fontSize: 32,
+    fontWeight: '500'
+  },
+  fullWidth: {
+    width: '100%'
+  }
+})
